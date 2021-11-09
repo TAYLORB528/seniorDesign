21498a27bd0eddd5ada4b3df62578a8a3250f10b
Taylor Barthel <taylorb528@gmail.com>
2021-11-09 16:33:08 -0500

    more route testing;

---
+++ Choosr/Choosr/Views/Home.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Choosr.Views
+{
+    public partial class Home : ContentPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            await Shell.Current.GoToAsync(nameof(ModeSelection));
+        }
+    }
+}
---
+++ Choosr/Choosr/Views/ModeSelection.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Choosr.Views
+{
+    public partial class ModeSelection : ContentPage
+    {
+        public ModeSelection()
+        {
+            InitializeComponent();
+        }
+    }
+}
